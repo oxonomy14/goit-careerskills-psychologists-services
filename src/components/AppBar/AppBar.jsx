7d3c025423f815b624cfdba23e031ee86b389847
oxonomy14 <oxonomy14@gmail.com>
2025-09-11 17:58:33 +0700

    add fluid layout for mobile screen

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -7,19 +7,24 @@ const AppBar = () => {
     <header className={css.header}>
       <Container>
         <div className={css.wrapper}>
-      
-            <div className={css.logo}>
-           <p className={css.logoText}>psychologists.<span>services</span></p>
-            </div>
-            <div className={css.navigation}>
-              <Navigation />
-            </div>
-            <div className={css.registration}>
-              <button className={css.btnLogin}>Log In</button>
-              <button className={css.btnRegistration}>Registration</button>
-            </div>
+          <div className={css.logo}>
+            <p className={css.logoText}>
+              psychologists.<span>services</span>
+            </p>
           </div>
-
+          <div className={css.navigation}>
+            <Navigation />
+          </div>
+          <div className={css.registration}>
+            <button className={css.btnLogin}>Log In</button>
+            <button className={css.btnRegistration}>Registration</button>
+          </div>
+          <button className={css.btnBurger}>
+            <svg width={31} height={32} className={css.burgerIcon}>
+              <use href="/icons/sprite.svg#icon-burger"></use>
+            </svg>
+          </button>
+        </div>
       </Container>
     </header>
   );
